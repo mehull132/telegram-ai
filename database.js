88e85cbe13cd1dc5d13e8f3decb01dc58eb8b8e6
mehull132 <135197471+mehull132@users.noreply.github.com>
2026-06-02 17:30:19 +0530

    Update database.js

--- database.js
+++ database.js
@@ -3,18 +3,6 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./memory.db');
 
 db.serialize(() => {
-  db.run(`
-    CREATE TABLE IF NOT EXISTS users (
-      id INTEGER PRIMARY KEY,
-      name TEXT
-    )
-  `);
-});
-
-console.log("DATABASE CONNECTED");
-
-module.exports = db;
-db.serialize(() => {
 
   db.run(`
     CREATE TABLE IF NOT EXISTS users (
@@ -33,3 +21,7 @@ db.serialize(() => {
   `);
 
 });
+
+console.log("DATABASE CONNECTED");
+
+module.exports = db;
