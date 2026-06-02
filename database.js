29f2c991f449b070a3817758e947e0d4664ea6d2
mehull132 <135197471+mehull132@users.noreply.github.com>
2026-06-02 17:24:29 +0530

    Update database.js

--- database.js
+++ database.js
@@ -14,3 +14,22 @@ db.serialize(() => {
 console.log("DATABASE CONNECTED");
 
 module.exports = db;
+db.serialize(() => {
+
+  db.run(`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY,
+      name TEXT
+    )
+  `);
+
+  db.run(`
+    CREATE TABLE IF NOT EXISTS memories (
+      user_id TEXT,
+      memory_key TEXT,
+      memory_value TEXT,
+      PRIMARY KEY(user_id, memory_key)
+    )
+  `);
+
+});
